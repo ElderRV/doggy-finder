a1e5c8c76a9587c77422223e001d2603b80166e0
ElderRV <elder.ramirez2042@alumnos.udg.mx>
2025-01-19 15:57:52 -0600

    arreglar tipos

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { TypeCoordenadas } from "@/types"
+import { Coordenadas } from "@/types"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
-export async function obtenerUbicacion(): Promise<TypeCoordenadas | null> {
+export async function obtenerUbicacion(): Promise<Coordenadas | null> {
     return await new Promise((res) => {
         navigator.geolocation.getCurrentPosition(geolocation => {
             const { longitude: longitud, latitude: latitud } = geolocation.coords;
